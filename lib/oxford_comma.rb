da2483dcafd550667078b2bc7219a4ba19bd96c8
John Grubbs <johnmlowery@gmail.com>
2018-08-21 01:30:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,12 @@
 def oxford_comma(array)
-  newArray = []
-array.each do |name|
-  newArray.push array[0]
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array_item)
+    return array.join(", ")
   end
-  return newArray
 end
